cfe8c98d8834fbf4226ee0c2e305cef1054d93dc
gwacky <89045948+gwacky@users.noreply.github.com>
2024-10-31 18:54:12 -0500

    Create test.js

---
+++ Chapter3/test.js
@@ -0,0 +1,29 @@
+// Grace Cunningham 10/31/24
+
+// A variable to store your full name
+let fullName = "Grace Cunningham";
+console.log(fullName);
+
+// A variable to store your desired annual salary
+let desiredAnnualSalary = 55000;
+console.log(desiredAnnualSalary);
+
+// A variable to store your veteran status (hint: either you are a veteran or you are not a veteran)
+let veteranStatus = false;
+console.log(veteranStatus);
+
+// An array to store the names of three of your friends.
+let friends = ['Cory', 'Mj', 'Gia'];
+console.log(friends);
+
+// An array to store the value of the desired annual salary for your three friends
+let friendsDesiredAnnualSalary = [70000, 40000, 80000];
+console.log(friendsDesiredAnnualSalary);
+
+// A literal object to store the first name, last name, and desired annual salary of yet another friend
+let friend = {
+    firstName: "Hunter",
+    lastName: "Jones",
+    desiredAnnualSalary: 150000,
+};
+console.log(friend);
